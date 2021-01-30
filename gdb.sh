dadc62ac7ca2a7243d17c982e05680893ae67649
Stefan Huber <shuber@sthu.org>
2021-01-30 19:48:42 +0100

    gdb: Check if gdb-dashboard is installed

--- gdb.sh
+++ gdb.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-GDBARGS=""
+set -eu
 
+GDBBIN="gdb"
+GDBARGS=""
+WITH_DASHBOARD=0
+
+# If gdb-dashboard is installed, use it
+if command -v gdb-dashboard &> /dev/null; then
+    GDBBIN="gdb-dashboard"
+    WITH_DASHBOARD=1
+fi
+
+# Check if gdbinit ships gdb-dashboard
 if [ -e ~/.gdbinit ] && grep -q dashboard ~/.gdbinit; then
+    WITH_DASHBOARD=1
+fi
+
+# If we use dashboard then pass corresponding gdbinit file
+if [ "${WITH_DASHBOARD}" = "1" ]; then
     GDBARGS="${GDBARGS} -x ~/.vim/gdbinit-dashboard"
 fi
 
-exec gdb ${GDBARGS} "$@"
+exec ${GDBBIN} ${GDBARGS} "$@"
